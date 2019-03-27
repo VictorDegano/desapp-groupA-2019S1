18ca74dbde0d82da108f79fa61ee88f4901a76b1
Victor Degano <flame.el22@gmail.com>
2019-03-26 21:52:54 -0300

    Fix Application de SpringBoot
    Se agrego la clase Application del proyecto el build del maven.

---
+++ src/main/java/model/DesappGroupABackendApplication.java
@@ -0,0 +1,13 @@
+package model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesappGroupABackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesappGroupABackendApplication.class, args);
+	}
+
+}
