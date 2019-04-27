4778d2ffd6ccf05d5058b32e0477b1c77b1d9ec2
IvanJara <ivanjaratamargo@gmail.com>
2019-04-27 18:50:46 -0300

    added Canasta States

---
+++ desapp-groupA-2019S1-backend/src/main/java/ar/edu/unq/desapp/grupoa/model/canasta_states/CanastaState.java
@@ -0,0 +1,20 @@
+package ar.edu.unq.desapp.grupoa.model.canasta_states;
+
+import ar.edu.unq.desapp.grupoa.model.Canasta;
+
+public abstract class CanastaState {
+
+    protected Canasta canasta;
+
+    protected void setCanasta(Canasta aCanasta) {
+        this.canasta = aCanasta;
+    }
+
+    public Boolean isInPreparationCanasta(){
+        return false;
+    }
+
+    public Boolean isCloseCanasta(){
+        return false;
+    }
+}
