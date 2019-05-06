511f9f71117399d04998e451b3c2b6239795bb49
IvanJara <ivanjaratamargo@gmail.com>
2019-05-05 21:45:22 -0300

    adding constructor, ownAGood and confirmUser implementations in 2 subclasses

--- desapp-groupA-2019S1-backend/src/main/java/ar/edu/unq/desapp/grupoa/model/event/canasta/state/CloseCanasta.java
+++ desapp-groupA-2019S1-backend/src/main/java/ar/edu/unq/desapp/grupoa/model/event/canasta/state/CloseCanasta.java
@@ -1,10 +1,31 @@
 package ar.edu.unq.desapp.grupoa.model.event.canasta.state;
 
 
+import ar.edu.unq.desapp.grupoa.exception.event.CanastaCloseException;
+import ar.edu.unq.desapp.grupoa.exception.event.ConfirmAsistanceException;
+import ar.edu.unq.desapp.grupoa.model.event.canasta.Canasta;
+import ar.edu.unq.desapp.grupoa.model.event.canasta.CanastaGood;
+import ar.edu.unq.desapp.grupoa.model.user.User;
+
 public class CloseCanasta extends CanastaState {
+
+    public CloseCanasta(Canasta aCanasta) {
+        super(aCanasta);
+    }
+
+    @Override
+    public void ownAGood(User user, CanastaGood good) {
+            throw new CanastaCloseException(this.getCanasta().getName(),user.getFirstName());
+    }
 
     @Override
     public Boolean isCloseCanasta(){
         return true;
+    }
+
+    @Override
+    public void confirmUser(User userToConfirmAssistance) {
+            throw new ConfirmAsistanceException(this.getCanasta().getName(),userToConfirmAssistance.getFirstName());
+
     }
 }
