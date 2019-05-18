eea329cd6b6ec47786d1bdccc60214a0aa5f5a31
Ivan Dominikow <Dominikowivan@gmail.com>
2019-05-18 14:28:00 -0300

    baquitaComunitaria and Templates for both baquitas

---
+++ desapp-groupA-2019S1-backend/src/main/java/ar/edu/unq/desapp/grupoa/model/event/createstrategy/CreateBaquitaComunitariaStrategy.java
@@ -0,0 +1,23 @@
+package ar.edu.unq.desapp.grupoa.model.event.createstrategy;
+
+import ar.edu.unq.desapp.grupoa.model.event.Event;
+import ar.edu.unq.desapp.grupoa.model.event.EventType;
+import ar.edu.unq.desapp.grupoa.model.event.Guest;
+import ar.edu.unq.desapp.grupoa.model.event.Template;
+import ar.edu.unq.desapp.grupoa.model.event.baquita.BaquitaComunitary;
+import ar.edu.unq.desapp.grupoa.model.user.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class CreateBaquitaComunitariaStrategy implements CreateEventStrategy{
+    @Override
+    public boolean hasToHandleCreateEvent(EventType aEventType) {
+        return aEventType.equals(EventType.BAQUITA_COMUNITARY);
+    }
+
+    @Override
+    public Event createEvent(String name, User organizer, List<Guest> guests, LocalDateTime limitTime, Template template) {
+        return BaquitaComunitary.createWithATemplate(name, organizer, guests, template);
+    }
+}
