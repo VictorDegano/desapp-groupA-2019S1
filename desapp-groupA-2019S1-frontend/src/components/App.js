c24204155b019ff4d8acf79dd69d5854871cc654
jaraivan <ivanjaratamargo@gmail.com>
2019-06-02 19:25:14 -0300

    adding a getEventos function that returns an array, this is a dummy to api request data, will be override to that request added the EventsComponent to show 3 tables

--- desapp-groupA-2019S1-frontend/src/components/App.js
+++ desapp-groupA-2019S1-frontend/src/components/App.js
@@ -1,11 +1,57 @@
 import React from "react";
 import EventList from "../containers/EventList";
 import {withRouter} from "react-router-dom";
+import NavigationBar from "./NavigationBar";
+import EventsComponent from "./EventsComponent";
 
 
+function getEventos(){
+    return [
+        {
+            "id":1,
+            "name":"La fiesta de Ivan",
+            "organizer":{
+                "id":1,
+                "firstName":"Ivan",
+                "lastName":"Dominikow",
+                "email":"ivanDominikow@acaradeperro.com"
+            },
+            "type":"FIESTA",
+            "quantityOfGuest":0
+        },
+        {
+            "id":2,
+            "name":"IvanFest",
+            "organizer":{
+                "id":1,
+                "firstName":"Ivan",
+                "lastName":"Dominikow",
+                "email":"ivanDominikow@acaradeperro.com"
+            },
+            "type":"FIESTA",
+            "quantityOfGuest":0
+        },
+        {
+            "id":3,
+            "name":"Canasteando 2.0",
+            "organizer":{
+                "id":1,
+                "firstName":"Ivan",
+                "lastName":"Dominikow",
+                "email":"ivanDominikow@acaradeperro.com"
+            },
+            "type":"CANASTA",
+            "quantityOfGuest":0
+        }
+    ]
+
+};
 const App = () => (
   <div>
-    Hello World! Its The Home Page!
+    <NavigationBar/>
+    <EventsComponent title={"Mis eventos en curso:"} arrayDeEventos={getEventos()}/>
+    <EventsComponent title={"Mis ultimos eventos:"} arrayDeEventos={getEventos()}/>
+    <EventsComponent title={"Los eventos mas populares:" } arrayDeEventos={getEventos()}/>
     <EventList/>
   </div>
 );
