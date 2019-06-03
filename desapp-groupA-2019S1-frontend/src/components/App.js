3badc8d858690b50a1a3880eff94bd42dd075caf
jaraivan <ivanjaratamargo@gmail.com>
2019-06-02 21:26:55 -0300

    changing App to a Component adding the componentWillMount method and changing EventsComponents props to the internal state of the App

--- desapp-groupA-2019S1-frontend/src/components/App.js
+++ desapp-groupA-2019S1-frontend/src/components/App.js
@@ -1,59 +1,50 @@
 import React from "react";
-import EventList from "../containers/EventList";
 import {withRouter} from "react-router-dom";
 import NavigationBar from "./NavigationBar";
 import EventsComponent from "./EventsComponent";
+import EventApi from "../api/EventApi";
 
+class App extends React.Component {
 
-function getEventos(){
-    return [
-        {
-            "id":1,
-            "name":"La fiesta de Ivan",
-            "organizer":{
-                "id":1,
-                "firstName":"Ivan",
-                "lastName":"Dominikow",
-                "email":"ivanDominikow@acaradeperro.com"
-            },
-            "type":"FIESTA",
-            "quantityOfGuest":0
-        },
-        {
-            "id":2,
-            "name":"IvanFest",
-            "organizer":{
-                "id":1,
-                "firstName":"Ivan",
-                "lastName":"Dominikow",
-                "email":"ivanDominikow@acaradeperro.com"
-            },
-            "type":"FIESTA",
-            "quantityOfGuest":0
-        },
-        {
-            "id":3,
-            "name":"Canasteando 2.0",
-            "organizer":{
-                "id":1,
-                "firstName":"Ivan",
-                "lastName":"Dominikow",
-                "email":"ivanDominikow@acaradeperro.com"
-            },
-            "type":"CANASTA",
-            "quantityOfGuest":0
+    constructor(props){
+        super(props);
+        this.state = {
+            eventosEnCurso: [],
+            misUltimosEventos: []
         }
-    ]
 
-};
-const App = () => (
-  <div>
-    <NavigationBar/>
-    <EventsComponent title={"Mis eventos en curso:"} arrayDeEventos={getEventos()}/>
-    <EventsComponent title={"Mis ultimos eventos:"} arrayDeEventos={getEventos()}/>
-    <EventsComponent title={"Los eventos mas populares:" } arrayDeEventos={getEventos()}/>
-    <EventList/>
-  </div>
-);
+    }
+
+    //Ocurre antes de que el componente se monte(o complete de montarse)
+    componentWillMount(){
+        // console.log('componentWilMount()');
+        var eventApi = new EventApi();
+
+        eventApi.getEventosEnCurso(1)
+            .then((response) => {
+                this.setState({
+                    eventosEnCurso: response.data,
+                });
+            });
+
+        eventApi.getMisUltimosEventos(1)
+            .then((response) => {
+                this.setState({
+                    misUltimosEventos: response.data,
+                });
+            });
+    };
+
+    render(){
+        return <div>
+                <NavigationBar/>
+                <EventsComponent title={"Mis eventos en curso:"} arrayDeEventos={this.state.eventosEnCurso}/>
+                <EventsComponent title={"Mis ultimos eventos:"} arrayDeEventos={this.state.misUltimosEventos}/>
+                {/*<EventsComponent title={"Los eventos mas populares:" } arrayDeEventos={getEventos()}/>*/}
+            </div>
+
+    }
+}
+
 
 export default withRouter(App);
