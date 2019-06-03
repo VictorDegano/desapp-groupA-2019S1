cb7fc3c3a79c2c240ef361f2f9c919d210addd46
jaraivan <ivanjaratamargo@gmail.com>
2019-06-02 21:25:08 -0300

    added getEventosEnCurso and getMisUltimosEventos in the Event Api to use in the Home Page

--- desapp-groupA-2019S1-frontend/src/api/EventApi.js
+++ desapp-groupA-2019S1-frontend/src/api/EventApi.js
@@ -20,13 +20,43 @@ class EventApi {
         return axios.get( API_CONFIG.endPoint + "fiesta/" + eventNumber, header);
     }
 
+    getEventosEnCurso(userId){
+        const accessToken= auth.getAccessToken();
+
+        const header = {
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type": API_CONFIG.contentType,
+                "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
+                "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
+            }
+        };
+
+        return axios.get( API_CONFIG.endPoint + "event/in_progress/" + userId, header);
+    }
+
+    getMisUltimosEventos(userId){
+        const accessToken= auth.getAccessToken();
+
+        const header = {
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type": API_CONFIG.contentType,
+                "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
+                "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
+            }
+        };
+
+        return axios.get( API_CONFIG.endPoint + "event/last_events/" + userId, header);
+    }
+
     fetchEvents(){
         // console.log('fetchEvents()');
-        return this.getEvent("1")
+        return this.getEventosEnCurso(1)
                    .then( (response) => {
-                            let events = this.getAllEvents();
-                            events.push(response.data);
-                            return events;
+                            // let events = this.getAllEvents();
+                            // events.push(response);
+                            return response.data;
                             })
                     .catch((error) => []);
                     //TODO: habria que pensar un mejor handleo.
