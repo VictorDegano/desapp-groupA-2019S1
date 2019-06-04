c587ae971f96a825bef97bc346f41ace73ea0f26
IvanJara <ivanjaratamargo@gmail.com>
2019-06-04 17:16:25 -0300

    deleting search, and search style

--- desapp-groupA-2019S1-frontend/src/components/NavigationBar.js
+++ desapp-groupA-2019S1-frontend/src/components/NavigationBar.js
@@ -29,30 +29,6 @@ const useStyles = makeStyles(theme => ({
             display: 'block',
         },
     },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        width: theme.spacing(7),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
     inputRoot: {
         color: 'inherit',
     },
@@ -167,18 +143,6 @@ function NavigationBar() {
                     <Typography className={classes.title} variant="h6" noWrap>
                         Eventeando
                     </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput,
-                            }}
-                        />
-                    </div>
                     <div className={classes.grow} />
                     <div className={classes.sectionDesktop}>
                         <IconButton color="inherit">
