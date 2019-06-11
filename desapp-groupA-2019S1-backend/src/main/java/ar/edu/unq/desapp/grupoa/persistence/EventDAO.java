311a46e37758969dac3acd699d09245dd3c5c495
VictorDegano <flame.el22@gmail.com>
2019-06-10 23:32:04 -0300

    Added logincontroller
    Added Login Controller to create and retrieve a user was login with google authentication.

--- desapp-groupA-2019S1-backend/src/main/java/ar/edu/unq/desapp/grupoa/persistence/EventDAO.java
+++ desapp-groupA-2019S1-backend/src/main/java/ar/edu/unq/desapp/grupoa/persistence/EventDAO.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
-
 import java.util.List;
 
 public interface EventDAO extends JpaRepository<Event, Integer>, QueryByExampleExecutor<Event> {
