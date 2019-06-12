8d43950bc4740e4aa3462a6a3b3277d0553b8e89
IvanJara <ivanjaratamargo@gmail.com>
2019-06-11 22:54:03 -0300

    automatic rearrange code on EventApi

--- desapp-groupA-2019S1-frontend/src/api/EventApi.js
+++ desapp-groupA-2019S1-frontend/src/api/EventApi.js
@@ -1,71 +1,70 @@
-import fiestaMock from "../resources/Fiesta.js";
 import axios from "axios";
-import {auth} from "../components/Root.js";
-import {API_CONFIG} from "./Configs/Api-config.js";
+import { auth } from "../components/Root";
+import { API_CONFIG } from "./Configs/Api-config";
 
 class EventApi {
-    
-    getEvent(eventNumber){
-        const accessToken= auth.getAccessToken();
+  getEvent(eventNumber) {
+    const accessToken = auth.getAccessToken();
 
-        const header = {
-            headers: {
-                "Authorization": `Bearer ${accessToken}`,
-                "Content-Type": API_CONFIG.contentType,
-                "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
-                "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
-            }
-        };
+    const header = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": API_CONFIG.contentType,
+        "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
+        "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
+      }
+    };
 
-        return axios.get( API_CONFIG.endPoint + "fiesta/" + eventNumber, header);
-    }
+    return axios.get(API_CONFIG.endPoint + "fiesta/" + eventNumber, header);
+  }
 
-    getEventosEnCurso(userId){
-        const accessToken= auth.getAccessToken();
+  getEventosEnCurso(userId) {
+    const accessToken = auth.getAccessToken();
 
-        const header = {
-            headers: {
-                "Authorization": `Bearer ${accessToken}`,
-                "Content-Type": API_CONFIG.contentType,
-                "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
-                "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
-            }
-        };
+    const header = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": API_CONFIG.contentType,
+        "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
+        "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
+      }
+    };
 
-        return axios.get( API_CONFIG.endPoint + "event/in_progress/" + userId, header);
-    }
+    return axios.get(
+      API_CONFIG.endPoint + "event/in_progress/" + userId,
+      header
+    );
+  }
 
-    getMisUltimosEventos(userId){
-        const accessToken= auth.getAccessToken();
+  getMisUltimosEventos(userId) {
+    const accessToken = auth.getAccessToken();
 
-        const header = {
-            headers: {
-                "Authorization": `Bearer ${accessToken}`,
-                "Content-Type": API_CONFIG.contentType,
-                "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
-                "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
-            }
-        };
+    const header = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": API_CONFIG.contentType,
+        "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
+        "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
+      }
+    };
 
-        return axios.get( API_CONFIG.endPoint + "event/last_events/" + userId, header);
-    }
+    return axios.get(
+      API_CONFIG.endPoint + "event/last_events/" + userId,
+      header
+    );
+  }
 
-    fetchEvents(){
-        // console.log('fetchEvents()');
-        return this.getEventosEnCurso(1)
-                   .then( (response) => {
-                            // let events = this.getAllEvents();
-                            // events.push(response);
-                            return response.data;
-                            })
-                    .catch((error) => []);
-                    //TODO: habria que pensar un mejor handleo.
-    }
-
-    getAllEvents(){
-        // console.log('getAllEvents()');
-        return fiestaMock.Fiestas;
-    }
+  fetchEvents() {
+    // console.log('fetchEvents()');
+    return this.getEventosEnCurso(1)
+      .then(response => {
+        // let events = this.getAllEvents();
+        // events.push(response);
+        return response.data;
+      })
+      .catch(error => []);
+    //TODO: habria que pensar un mejor handleo.
+  }
 }
 
 export default EventApi;
