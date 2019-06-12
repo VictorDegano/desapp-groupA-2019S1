62f43145215e4745f7667ede58c9f58f36ae289a
IvanJara <ivanjaratamargo@gmail.com>
2019-06-11 22:52:55 -0300

    restructured EventTable with react-bootstrap

--- desapp-groupA-2019S1-frontend/src/components/EventTable.js
+++ desapp-groupA-2019S1-frontend/src/components/EventTable.js
@@ -1,59 +1,41 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        width: '100%',
-        marginTop: theme.spacing(3),
-        overflowX: 'auto',
-    },
-    table: {
-        minWidth: 650,
-    },
-}));
+import React from "react";
+import Table from "react-bootstrap/Table";
 
 function createDataWithJson(jsonDeEvento) {
-    return { name: jsonDeEvento.name,eventType: jsonDeEvento.type,organizer: jsonDeEvento.organizer, guestsAmount: jsonDeEvento.guestsAmount};
+  return {
+    name: jsonDeEvento.name,
+    eventType: jsonDeEvento.type,
+    organizer: jsonDeEvento.organizer,
+    guestsAmount: jsonDeEvento.guestsAmount
+  };
 }
 function parseArrayToFunction(rowsArray) {
-    return rowsArray.flat(row=> createDataWithJson(row));
+  return rowsArray.flat(row => createDataWithJson(row));
 }
 
 function EventTable(props) {
-    const classes = useStyles();
-
-    return (
-        <Paper className={classes.root}>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Nombre</TableCell>
-                        <TableCell align="right">Tipo de evento</TableCell>
-                        <TableCell align="right">Organiza</TableCell>
-                        <TableCell align="right">Cantidad de invitados</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {parseArrayToFunction(props.arrayDeEventos).map(row => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.type}</TableCell>
-                            <TableCell align="right">{row.organizer.firstName}</TableCell>
-                            <TableCell align="right">{row.quantityOfGuest}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </Paper>
-    );
+  return (
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Tipo de evento</th>
+          <th>Organiza</th>
+          <th>Cantidad de invitados</th>
+        </tr>
+      </thead>
+      <tbody>
+        {parseArrayToFunction(props.arrayDeEventos).map(row => (
+          <tr key={row.name}>
+            <td>{row.name}</td>
+            <td>{row.type}</td>
+            <td>{row.organizer.firstName}</td>
+            <td>{row.quantityOfGuest}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 }
 
 export default EventTable;
