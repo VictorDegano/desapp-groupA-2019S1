823973625a3766746c504bd789edf0b95f1f1925
IvanJara <ivanjaratamargo@gmail.com>
2019-06-11 22:54:39 -0300

    automatic rearrange code on UserApi

--- desapp-groupA-2019S1-frontend/src/api/UserApi.js
+++ desapp-groupA-2019S1-frontend/src/api/UserApi.js
@@ -1,33 +1,32 @@
 import axios from "axios";
-import {auth} from "../components/Root.js";
-import {API_CONFIG} from "./Configs/Api-config.js";
+import { auth } from "../components/Root";
+import { API_CONFIG } from "./Configs/Api-config";
 
 class UserApi {
-    
-    getUser(userId){
-        const accessToken= auth.getAccessToken();
+  getUser(userId) {
+    const accessToken = auth.getAccessToken();
 
-        const header = {
-            headers: {
-                "Authorization": `Bearer ${accessToken}`,
-                "Content-Type": API_CONFIG.contentType,
-                "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
-                "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
-            }
-        };
+    const header = {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": API_CONFIG.contentType,
+        "Access-Control-Allow-Methods": API_CONFIG.allowMethods,
+        "Access-Control-Allow-Origin": API_CONFIG.allowOrigin
+      }
+    };
 
-        return axios.get( API_CONFIG.endPoint + "user/" + userId, header);
-    }
+    return axios.get(API_CONFIG.endPoint + "user/" + userId, header);
+  }
 
-    fetchUser(userId){
-        // console.log('fetchUser()');
-        return this.getUser(userId)
-                   .then( (response) => {
-                            return response.data;
-                            })
-                    .catch((error) => []);
-                    //TODO: habria que pensar un mejor handleo.
-    }
+  fetchUser(userId) {
+    // console.log('fetchUser()');
+    return this.getUser(userId)
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => []);
+    //TODO: habria que pensar un mejor handleo.
+  }
 }
 
 export default UserApi;
