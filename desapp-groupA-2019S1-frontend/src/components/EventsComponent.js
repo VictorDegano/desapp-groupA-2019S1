80ce800da092ecad6fd5b6d33af2f5cbeba6313d
IvanJara <ivanjaratamargo@gmail.com>
2019-06-11 22:57:25 -0300

    automatic rearrange code on EventComponent and changing class to function

--- desapp-groupA-2019S1-frontend/src/components/EventsComponent.js
+++ desapp-groupA-2019S1-frontend/src/components/EventsComponent.js
@@ -1,15 +1,13 @@
 import React from "react";
 import EventTable from "./EventTable";
 
-
-class EventsComponent extends React.Component{
-    render() {
-        return<div>
-            <h1>{this.props.title}</h1>
-            <EventTable arrayDeEventos={this.props.arrayDeEventos}/>
-        </div>
-    }
-
+function EventsComponent(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <EventTable arrayDeEventos={props.arrayDeEventos} />
+    </div>
+  );
 }
 
 export default EventsComponent;
