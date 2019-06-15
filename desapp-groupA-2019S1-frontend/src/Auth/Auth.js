6ad1f61df10c3f6a22bdcce94be44c86eca2c19e
IvanJara <ivanjaratamargo@gmail.com>
2019-06-15 12:19:17 -0300

    automatic rearrange code on Auth, Auth0-config,EndpointResolver, Api-config

--- desapp-groupA-2019S1-frontend/src/Auth/Auth.js
+++ desapp-groupA-2019S1-frontend/src/Auth/Auth.js
@@ -1,143 +1,152 @@
 import auth0 from "auth0-js";
-import {AUTH_CONFIG} from "./Auth0-config";
+import { AUTH_CONFIG } from "./Auth0-config";
 import history from "../history";
 
 export default class Auth {
-    userProfile;
+  userProfile;
 
-    auth0 = new auth0.WebAuth({
-        domain: AUTH_CONFIG.domain,
-        clientID: AUTH_CONFIG.clientID,
-        redirectUri: AUTH_CONFIG.redirectUri,
-        audience: "https://eventeando.auth.com/api",
-        responseType: "token id_token",
-        scope: "openid profile email"
+  auth0 = new auth0.WebAuth({
+    domain: AUTH_CONFIG.domain,
+    clientID: AUTH_CONFIG.clientID,
+    redirectUri: AUTH_CONFIG.redirectUri,
+    audience: "https://eventeando.auth.com/api",
+    responseType: "token id_token",
+    scope: "openid profile email"
+  });
+
+  login() {
+    // console.log('login()');
+    this.auth0.authorize();
+  }
+
+  logout() {
+    // console.log('logout()');
+    // Remove the user
+    this.userProfile = null;
+
+    // Remove isLoggedIn flag from localStorage
+    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+    localStorage.removeItem("email");
+    localStorage.removeItem("first_name");
+    localStorage.removeItem("last_name");
+
+    this.auth0.logout({
+      returnTo: window.location.origin
     });
 
-    login() {
-        // console.log('login()');
-        this.auth0.authorize();
+    // navigate to the home route
+    history.replace("/home");
+  }
+
+  setSession(authResult) {
+    // console.log('setSession()');
+    // Set isLoggedIn flag in localStorage
+    localStorage.setItem("isLoggedIn", "true");
+
+    // Set the time that the Access Token will expire at
+    let expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
+
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+    localStorage.setItem("email", authResult.email);
+    localStorage.setItem("first_name", authResult.given_name);
+    localStorage.setItem("last_name", authResult.family_name);
+  }
+
+  validSession(authResult) {
+    return authResult && authResult.accessToken && authResult.idToken;
+  }
+
+  resolveRenewSession(error, authResult) {
+    if (this.validSession(authResult)) {
+      this.setSession(authResult);
+    } else if (error) {
+      this.logout();
+      // console.log(error); //TODO: valdria la pena que vaya al home?
+      alert(
+        `Could not get a new token (${error.error}: ${error.error_description}).`
+      );
     }
+  }
 
-    logout() {
-        // console.log('logout()');
-        // Remove the user
-        this.userProfile = null;
+  renewSession() {
+    // console.log('renewSession()');
+    this.auth0.checkSession({}, (err, authResult) =>
+      this.resolveRenewSession(err, authResult)
+    );
+  }
 
-        // Remove isLoggedIn flag from localStorage
-        localStorage.removeItem("isLoggedIn");
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("id_token");
-        localStorage.removeItem("expires_at");
-        localStorage.removeItem("email");
-        localStorage.removeItem("first_name");
-        localStorage.removeItem("last_name");
+  isAuthenticated() {
+    // console.log('isAuthenticated()');
+    // Check whether the current time is past the
+    // access token's expiry time
+    let expiresAt = this.expiresAt;
+    return new Date().getTime() < expiresAt;
+  }
 
-        this.auth0.logout({
-            returnTo: window.location.origin
-        });
-
-        // navigate to the home route
-        history.replace("/home");
+  resolveHandleAuthentication(error, authResult) {
+    if (this.validSession(authResult)) {
+      this.setSession(authResult);
+    } else if (error) {
+      //TODO: Hay que contemplar que hacer en el caso que no se autentique,
+      //se podria informar con un modal y retornarlo a la pagina de inicion
+      alert(
+        `Could not be authenticated (${error.error}: ${error.error_description}).`
+      );
     }
+  }
 
-    setSession(authResult) {
-        // console.log('setSession()');
-        // Set isLoggedIn flag in localStorage
-        localStorage.setItem("isLoggedIn", "true");
+  handleAuthentication() {
+    // console.log('handleAuthentication()');
+    this.auth0.parseHash((err, authResult) =>
+      this.resolveHandleAuthentication(err, authResult)
+    );
+  }
 
-        // Set the time that the Access Token will expire at
-        let expiresAt = (authResult.expiresIn * 1000) + new Date().getTime();
-        
-        localStorage.setItem("access_token", authResult.accessToken);
-        localStorage.setItem("id_token", authResult.idToken);
-        localStorage.setItem("expires_at", expiresAt);
-        localStorage.setItem("email", authResult.email);
-        localStorage.setItem("first_name", authResult.given_name);
-        localStorage.setItem("last_name", authResult.family_name);
-    }
+  getAccessToken() {
+    // console.log('getAccessToken()');
+    return localStorage.getItem("access_token");
+  }
 
-    validSession(authResult){
-        return authResult && authResult.accessToken && authResult.idToken;
-    }
+  getIdToken() {
+    // console.log('getIdToken()');
+    return localStorage.getItem("id_token");
+  }
 
-    resolveRenewSession(error, authResult){
-        if (this.validSession(authResult)) {
-            this.setSession(authResult);
-        } else if (error) {
-            this.logout();
-            // console.log(error); //TODO: valdria la pena que vaya al home?
-            alert(`Could not get a new token (${error.error}: ${error.error_description}).`);
-        }
+  resolveGetProfile(callback, error, profile) {
+    if (profile) {
+      this.userProfile = profile;
     }
+    callback(error, profile);
+  }
 
-    renewSession() {
-        // console.log('renewSession()');
-        this.auth0.checkSession({}, (err, authResult) => this.resolveRenewSession(err, authResult) );
-    }
+  getProfile(cb) {
+    // console.log('getProfile()');
+    this.auth0.client.userInfo(
+      localStorage.getItem("access_token"),
+      (err, profile) => this.resolveGetProfile(cb, err, profile)
+    );
+  }
 
-    isAuthenticated() {
-        // console.log('isAuthenticated()');
-        // Check whether the current time is past the
-        // access token's expiry time
-        let expiresAt = this.expiresAt;
-        return new Date().getTime() < expiresAt;
-    }
-
-    resolveHandleAuthentication(error, authResult){
-        if(this.validSession(authResult)){
-            this.setSession(authResult);
-        } else if (error) {
-            //TODO: Hay que contemplar que hacer en el caso que no se autentique, 
-            //se podria informar con un modal y retornarlo a la pagina de inicion
-            alert(`Could not be authenticated (${error.error}: ${error.error_description}).`);
-        }
-    }
-
-    handleAuthentication() {
-        // console.log('handleAuthentication()');
-        this.auth0.parseHash((err, authResult) => this.resolveHandleAuthentication(err, authResult));
-    }
-
-    getAccessToken() {
-        // console.log('getAccessToken()');
-        return localStorage.getItem("access_token");
-    }
-    
-    getIdToken() {
-        // console.log('getIdToken()');
-        return localStorage.getItem("id_token");
-    }
-
-    resolveGetProfile(callback, error, profile){
-        if (profile) {
-          this.userProfile = profile;
-        }
-        callback(error, profile);
-    }
-
-    getProfile(cb) {
-        // console.log('getProfile()');
-        this.auth0
-            .client
-            .userInfo(
-                localStorage.getItem("access_token"), 
-                (err, profile) => this.resolveGetProfile(cb,err,profile));
-    }
-
-    //Esto es para evitar perder la referencia
-    constructor() {
-        this.resolveHandleAuthentication = this.resolveHandleAuthentication.bind(this);
-        this.validSession = this.validSession.bind(this);
-        this.resolveRenewSession = this.resolveRenewSession.bind(this);
-        this.login = this.login.bind(this);
-        this.logout = this.logout.bind(this);
-        this.handleAuthentication = this.handleAuthentication.bind(this);
-        this.isAuthenticated = this.isAuthenticated.bind(this);
-        this.setSession = this.setSession.bind(this);
-        this.getAccessToken = this.getAccessToken.bind(this);
-        this.getIdToken = this.getIdToken.bind(this);
-        this.renewSession = this.renewSession.bind(this);
-        this.getProfile = this.getProfile.bind(this);
-    }
+  //Esto es para evitar perder la referencia
+  constructor() {
+    this.resolveHandleAuthentication = this.resolveHandleAuthentication.bind(
+      this
+    );
+    this.validSession = this.validSession.bind(this);
+    this.resolveRenewSession = this.resolveRenewSession.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.setSession = this.setSession.bind(this);
+    this.getAccessToken = this.getAccessToken.bind(this);
+    this.getIdToken = this.getIdToken.bind(this);
+    this.renewSession = this.renewSession.bind(this);
+    this.getProfile = this.getProfile.bind(this);
+  }
 }
