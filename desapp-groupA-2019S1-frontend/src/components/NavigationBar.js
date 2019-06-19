f2c5a5574f97e07807ec559f61a9cd49a60ce1d1
IvanJara <ivanjaratamargo@gmail.com>
2019-06-18 23:01:16 -0300

    refactor over NavigationBar, updating links and adding new route, changing search part, still need to make it work to search on current events

--- desapp-groupA-2019S1-frontend/src/components/NavigationBar.js
+++ desapp-groupA-2019S1-frontend/src/components/NavigationBar.js
@@ -7,16 +7,21 @@ import FormControl from "react-bootstrap/es/FormControl";
 
 function NavigationBar() {
   return (
-    <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="#home">Eventeando</Navbar.Brand>
+    <Navbar variant="dark">
+      <Navbar.Brand href="/home" alt="Home">
+        Eventeando
+      </Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="/home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
+        <Nav.Link href="/newEvent">Crear Evento</Nav.Link>
       </Nav>
       <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-light">Search</Button>
+        <FormControl
+          type="text"
+          placeholder="Buscar Evento"
+          className="mr-sm-2"
+        />
+        <Button variant="outline-light">Buscar</Button>
       </Form>
     </Navbar>
   );
