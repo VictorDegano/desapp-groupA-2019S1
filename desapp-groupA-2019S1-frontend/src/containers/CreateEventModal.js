4fc2392c07078b60dec899eb56eb3d6957ab9a70
IvanJara <ivanjaratamargo@gmail.com>
2019-06-29 00:00:50 -0300

    Adding the CreateEventModal container, still need some changes

---
+++ desapp-groupA-2019S1-frontend/src/containers/CreateEventModal.js
@@ -0,0 +1,262 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+// I18n Hook
+import { withTranslation } from "react-i18next";
+// Bootstrap
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "react-datepicker/dist/react-datepicker-cssmodules.css";
+import "../css/ProfileEdition.css";
+import Alert from "react-bootstrap/Alert";
+//Actions
+import { updateLoggedUser } from "../actions/UserActions";
+import { closeCreateEventModal } from "../actions/ModalViewActions";
+// API's
+import UserApi from "../api/UserApi";
+import {Redirect} from "react-router";
+
+class CreateEventModal extends Component {
+  static propTypes = {
+    updateLoggedUser: PropTypes.func.isRequired,
+    closeCreateEventModal: PropTypes.func.isRequired,
+    show: PropTypes.bool,
+    loggedUser: PropTypes.object
+  };
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      startDate: new Date(),
+      showAlert: false,
+      messageAlert: ""
+    };
+    this.handleClose = this.handleClose.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleBornDateChange = this.handleBornDateChange.bind(this);
+    this.checkValidation = this.checkValidation.bind(this);
+    this.handleDismiss = this.handleDismiss.bind(this);
+  }
+
+  componentWillMount() {
+    const loggedUser = this.props.loggedUser;
+
+    this.setState({
+      startDate: new Date(loggedUser.bornDate)
+    });
+  }
+
+  handleClose() {
+    // console.log("handleClose()");
+    this.props.closeCreateEventModal();
+    this.setState({
+      startDate: new Date(),
+      showAlert: false,
+      messageAlert: ""
+    });
+  }
+
+  handleBornDateChange(date) {
+    // this.selectedDate = date;
+    this.setState({
+      startDate: date
+    });
+  }
+
+  handleSave(event) {
+    // console.log("handleSave()");
+    event.preventDefault();
+
+    if (this.checkValidation(event)) {
+      const loggedUser = this.props.loggedUser;
+
+      const userToSave = {
+        id: loggedUser.id,
+        fistName: event.target[1].value,
+        lastName: event.target[2].value,
+        email: loggedUser.email,
+        bornDate: this.state.startDate.toISOString()
+      };
+
+      const userToStateUpdate = {
+        id: loggedUser.id,
+        fistName: event.target[1].value,
+        lastName: event.target[2].value,
+        email: loggedUser.email,
+        bornDate: this.state.startDate.toJSON()
+      };
+
+      const userApi = new UserApi();
+
+      userApi
+        .putUser(userToSave)
+        .then(response => {
+          this.props.updateLoggedUser(userToStateUpdate);
+          this.handleClose();
+        })
+        .catch(error => {
+          alert("An error has occurred, please try again");
+        });
+    } else {
+      event.stopPropagation();
+      this.setState({
+        showAlert: true,
+        messageAlert: this.getError(event)
+      });
+    }
+  }
+
+  checkValidation(event) {
+    return (
+      event.target[1].value !== "" &&
+      event.target[2].value !== "" &&
+      (this.state.startDate !== null &&
+        this.state.startDate <= new Date() &&
+        this.state.startDate >= new Date("01/01/1900"))
+    );
+  }
+
+  getError(event) {
+    let error = "";
+    if (event.target[1].value === "") {
+      error += error !== "" ? "\n" : "";
+      error += "El nombre no puede ser vacio";
+    }
+    if (event.target[2].value === "") {
+      error += error !== "" ? "\n" : "";
+      error += "El Apellido no puede ser vacio";
+    }
+    if (
+      this.state.startDate === null ||
+      (this.state.startDate > new Date()) |
+        (this.state.startDate < new Date("01/01/1900"))
+    ) {
+      error += error !== "" ? "\n" : "";
+      error += "La fecha tiene que ser menor a hoy y mayor a 01/01/1990";
+    }
+
+    return error;
+  }
+
+  handleDismiss() {
+    this.setState({
+      showAlert: false,
+      messageAlert: ""
+    });
+  }
+
+  render() {
+    const { t } = this.props;
+    const loggedUser = this.props.loggedUser;
+    const show = this.props.show;
+    const { validated } = this.state;
+
+    return (
+      <>
+        <Modal show={show} onHide={this.handleClose}>
+          <Alert
+            variant="danger"
+            show={this.state.showAlert}
+            onClose={this.handleDismiss}
+            dismissible
+          >
+            <Alert.Heading>¡Error!</Alert.Heading>
+            <p>{this.state.messageAlert}</p>
+          </Alert>
+          <Form onSubmit={this.handleSave} noValidate validated={validated}>
+            <Modal.Header closeButton>
+              <Modal.Title>{t("createEventModal->title")}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form.Group controlId="formBasicFirstName">
+                <Form.Label>
+                  {t("createEventModal->form->firstName")}
+                </Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder={t(
+                    "createEventModal->form->firstNamePlaceholder"
+                  )}
+                  defaultValue={loggedUser.fistName}
+                />
+              </Form.Group>
+              <Form.Group controlId="createEventForm.EventTypes">
+                <Form.Label>Tipo de Evento</Form.Label>
+                <Form.Control as="select">
+                  <option>Fiesta</option>
+                  <option>Canasta</option>
+                  <option>Baquita Comunitaria</option>
+                  <option>Baquita Representantes</option>
+                </Form.Control>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicBornDate">
+                <Form.Label>{t("createEventModal->form->bornDate")}</Form.Label>
+                <div className="containerDatePicker">
+                  <DatePicker
+                    required
+                    className="Form.Control"
+                    minDate={new Date("01/01/1900")}
+                    maxDate={new Date()}
+                    selected={this.state.startDate}
+                    onChange={this.handleBornDateChange}
+                    dateFormat={t("formatter->date")}
+                    placeholderText={t(
+                      "createEventModal->form->bornDatePlaceholder"
+                    )}
+                    showYearDropdown
+                    scrollableYearDropdown
+                    yearDropdownItemNumber={80}
+                    fixedHeight
+                  />
+                </div>
+              </Form.Group>
+              <Form.Group controlId="createEventForm.Organizer">
+                <Form.Label>Organizador</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  // placeholder={t("createEventModal->form->emailPlaceholder")}
+                  disabled
+                  defaultValue={loggedUser.fistName + " " + loggedUser.lastName}
+                />
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={this.handleClose}>
+                {t("createEventModal->buttons->close")}
+              </Button>
+              <Button variant="primary" type="submit">
+                {t("createEventModal->buttons->save")}
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal>
+      </>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // console.log('mapStateToProps()')
+  return {
+    show: state.ModalViewReducer.modalCreateEventState,
+    loggedUser: state.UserReducer.loggedUser
+  };
+}
+
+const mapDispatchToProps = dispatch => ({
+  updateLoggedUser: user => dispatch(updateLoggedUser(user)),
+  closeCreateEventModal: () => dispatch(closeCreateEventModal())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+  // ModalViewActions,
+  // UserActions
+)(withTranslation()(CreateEventModal));
