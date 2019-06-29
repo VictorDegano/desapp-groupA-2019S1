3c6ea098e14f6c17d52dc2b44063d176e6a266bc
IvanJara <ivanjaratamargo@gmail.com>
2019-06-29 00:03:04 -0300

    Adding 2 new ModalViewActions openCreateEventModal and closeCreateEventModal

--- desapp-groupA-2019S1-frontend/src/actions/ModalViewActions.js
+++ desapp-groupA-2019S1-frontend/src/actions/ModalViewActions.js
@@ -1,4 +1,4 @@
-import * as ACTION_TYPE from "../actions/Action_Types/ModalView_Types.js";
+import * as ACTION_TYPE from "./Action_Types/ModalView_Types";
 
 //Todas las funciones de acciones que hacemos llevan un type que es la accion que realiza y luego la informacion
 export function openProfileEdition() {
@@ -16,5 +16,23 @@ export function closeProfileEdition() {
   //Las acciones devuelve un objeto
   return {
     type: ACTION_TYPE.CLOSE_PROFILE_EDITION
+  };
+}
+
+export function openCreateEventModal() {
+  // console.log("closeProfileEdition()");
+
+  //Las acciones devuelve un objeto
+  return {
+    type: ACTION_TYPE.OPEN_CREATE_EVENT_MODAL
+  };
+}
+
+export function closeCreateEventModal() {
+  // console.log("closeProfileEdition()");
+
+  //Las acciones devuelve un objeto
+  return {
+    type: ACTION_TYPE.CLOSE_CREATE_EVENT_MODAL
   };
 }
