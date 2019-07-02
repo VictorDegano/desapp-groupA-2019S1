1f04adeac104016550a30e1fcd9cbd8bf334f7a1
IvanJara <ivanjaratamargo@gmail.com>
2019-07-02 11:47:13 -0300

    replacing CreateEventModal with EventViewer code

--- desapp-groupA-2019S1-frontend/src/containers/CreateEventModal.js
+++ desapp-groupA-2019S1-frontend/src/containers/CreateEventModal.js
@@ -16,6 +16,11 @@ import Badge from "react-bootstrap/Badge";
 import { closeCreateEventModal } from "../actions/ModalViewActions";
 // css
 import "../css/ProfileEdition.css";
+import UserApi from "../api/UserApi";
+import { updateLoggedUser } from "../actions/UserActions";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import EventApi from "../api/EventApi";
 
 class CreateEventModal extends Component {
   static propTypes = {
@@ -24,7 +29,9 @@ class CreateEventModal extends Component {
     event: PropTypes.shape({
       eventName: PropTypes.string.isRequired,
       creationDate: PropTypes.string.isRequired
-    }).isRequired
+    }).isRequired,
+    loggedUser: PropTypes.object,
+    updateLoggedUser: PropTypes.func.isRequired
   };
 
   constructor(props, context) {
@@ -35,6 +42,63 @@ class CreateEventModal extends Component {
     this.getConfirmationStateTraslation = this.getConfirmationStateTraslation.bind(
       this
     );
+    this.handleSave = this.handleSave.bind(this);
+    this.state = {
+      eventName: "FiestaExample",
+      creationDate: new Date(),
+      goods: [],
+      guests: [
+        {
+          confirmAsistance: "",
+          firstName: "",
+          guestId: 0,
+          lastName: "",
+          mail: "",
+          userId: 0
+        }
+      ],
+      id: 0,
+      organizer: this.props.loggedUser,
+      quantityOfGuest: 0,
+      status: "OPEN",
+      type: "Fiesta",
+      confirmationDay: new Date()
+    };
+  }
+
+  componentDidMount() {
+    const loggedUser = this.props.loggedUser;
+
+    if (this.props.loggedUser === null) {
+      console.log("EL LOGGED USER ES NULL");
+      const userApi = new UserApi();
+      userApi.fetchUser(localStorage.getItem("id")).then(user => {
+        this.props.updateLoggedUser(user);
+        this.setState({
+          newEvent: {
+            organizer: {
+              startDate: new Date(user.bornDay),
+              firstName: user.firstName,
+              lastName: user.lastName,
+              email: user.email
+            }
+          }
+        });
+      });
+    } else {
+      console.log("EL LOGGED USER NO ES NULL");
+      this.setState({
+        firstName: loggedUser.firstName,
+        newEvent: {
+          organizer: {
+            startDate: loggedUser.bornDate,
+            firstName: loggedUser.firstName,
+            lastName: loggedUser.lastName,
+            email: loggedUser.email
+          }
+        }
+      });
+    }
   }
 
   getEventTime() {
@@ -81,77 +145,140 @@ class CreateEventModal extends Component {
     this.props.closeCreateEventModal();
   }
 
+  handleSave(event) {
+    // console.log("handleSave()");
+    event.preventDefault();
+    const eventApi = new EventApi();
+    console.log(event);
+    eventApi
+      .createEvent(event)
+      .then(response => console.log(response))
+      .catch(e => console.log(e));
+  }
+
   render() {
     const { t } = this.props;
     const show = this.props.show;
     const event = this.props.event;
+    const { validated } = this.state;
     return (
       <>
         <Modal show={show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>{event.eventName}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form.Label>{t("eventView->status")}</Form.Label>
-            <Form.Control plaintext readOnly defaultValue={event.status} />
-            <Form.Label>{t("eventView->organizer")}</Form.Label>
-            <Form.Control
-              plaintext
-              readOnly
-              defaultValue={
-                event.organizer.fistName + " " + event.organizer.lastName
-              }
-            />
-            <Form.Label>{t("eventView->creationDate")}</Form.Label>
-            <div className="containerDatePicker">
-              <DatePicker
+          <Form onSubmit={this.handleSave} noValidate validated={validated}>
+            <Modal.Header closeButton>
+              <Modal.Title>Create Event</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Row>
+                <Col>
+                  <Form.Label className="h5 text-center">
+                    Event Name:
+                  </Form.Label>
+                </Col>
+                <Col>
+                  <Form.Control plaintext defaultValue={this.state.eventName} />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Label>Type</Form.Label>
+                </Col>
+                <Col>
+                  <Form.Control as="select">
+                    <option>Fiesta</option>
+                    <option>Canasta</option>
+                    <option>Baquita Comunitaria</option>
+                    <option>Baquita Representantes</option>
+                    plaintext readOnly defaultValue={this.state.type}
+                  </Form.Control>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Label>{t("eventView->organizer")}</Form.Label>
+                </Col>
+                <Col>
+                  <Form.Control
+                    plaintext
+                    readOnly
+                    defaultValue={this.state.organizer.fistName}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Form.Label>{t("eventView->creationDate")}</Form.Label>
+                </Col>
+                <Col>
+                  <div className="containerDatePicker">
+                    <DatePicker
+                      readOnly
+                      disabled
+                      className="Form.Control"
+                      minDate={new Date("01/01/1900")}
+                      maxDate={new Date()}
+                      selected={this.state.creationDate}
+                      dateFormat={t("formatter->date")}
+                      showYearDropdown
+                      scrollableYearDropdown
+                      yearDropdownItemNumber={80}
+                      fixedHeight
+                    />
+                  </div>
+                </Col>
+              </Row>
+              <Form.Label>Confirmation Day</Form.Label>
+              <div className="containerDatePicker">
+                <DatePicker
+                  className="Form.Control"
+                  minDate={new Date()}
+                  maxDate={new Date("12/12/2020")}
+                  selected={this.state.confirmationDay}
+                  dateFormat={t("formatter->date")}
+                  showYearDropdown
+                  scrollableYearDropdown
+                  yearDropdownItemNumber={80}
+                  fixedHeight
+                />
+              </div>
+              <Form.Label>{t("eventView->guestQuantity")}</Form.Label>
+              <Form.Control
+                plaintext
                 readOnly
-                disabled
-                className="Form.Control"
-                minDate={new Date("01/01/1900")}
-                maxDate={new Date()}
-                selected={this.getEventTime()}
-                dateFormat={t("formatter->date")}
-                showYearDropdown
-                scrollableYearDropdown
-                yearDropdownItemNumber={80}
-                fixedHeight
+                defaultValue={this.state.quantityOfGuest}
               />
-            </div>
-            <Form.Label>{t("eventView->guestQuantity")}</Form.Label>
-            <Form.Control
-              plaintext
-              readOnly
-              defaultValue={event.quantityOfGuest}
-            />
-            <Form.Label>{t("eventView->guest")}</Form.Label>
-            <ListGroup as="ul" variant="flush">
-              {event.guests.map(guest => {
-                return (
-                  <ListGroup.Item
-                    key={guest.firstName + guest.email + guest.lastName}
-                    as="li"
-                  >
-                    <p>
-                      {guest.firstName + " " + guest.lastName}
-                      <Badge
-                        variant={this.getBadgeColour(guest.confirmAsistance)}
-                      >
-                        {this.getConfirmationStateTraslation(
-                          guest.confirmAsistance
-                        )}
-                      </Badge>
-                    </p>
-                  </ListGroup.Item>
-                );
-              })}
-            </ListGroup>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              {t("eventView->button->close")}
-            </Button>
-          </Modal.Footer>
+              <Form.Label>{t("eventView->guest")}</Form.Label>
+              <ListGroup as="ul" variant="flush">
+                {this.state.guests.map(guest => {
+                  return (
+                    <ListGroup.Item
+                      key={guest.firstName + guest.email + guest.lastName}
+                      as="li"
+                    >
+                      <p>
+                        {guest.firstName + " " + guest.lastName}
+                        <Badge
+                          variant={this.getBadgeColour(guest.confirmAsistance)}
+                        >
+                          {this.getConfirmationStateTraslation(
+                            guest.confirmAsistance
+                          )}
+                        </Badge>
+                      </p>
+                    </ListGroup.Item>
+                  );
+                })}
+              </ListGroup>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" type="submit">
+                {t("profileEditionModal->buttons->save")}
+              </Button>
+              <Button variant="secondary" onClick={this.handleClose}>
+                {t("eventView->button->close")}
+              </Button>
+            </Modal.Footer>
+          </Form>
         </Modal>
       </>
     );
@@ -162,12 +289,14 @@ function mapStateToProps(state) {
   // console.log('mapStateToProps()')
   return {
     show: state.ModalViewReducer.modalCreateEventState,
-    event: state.ModalViewReducer.event
+    event: state.ModalViewReducer.event,
+    loggedUser: state.UserReducer.loggedUser
   };
 }
 
 const mapDispatchToProps = dispatch => ({
-  closeCreateEventModal: () => dispatch(closeCreateEventModal())
+  closeCreateEventModal: () => dispatch(closeCreateEventModal()),
+  updateLoggedUser: user => dispatch(updateLoggedUser(user))
 });
 
 export default connect(
