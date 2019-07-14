cc6c37b4f1483a86090cda21c582e01bc815b48c
Victor Degano <flame.el22@gmail.com>
2019-07-14 00:41:10 -0300

    Fixes on loses files

--- desapp-groupA-2019S1-frontend/src/actions/ModalViewActions.js
+++ desapp-groupA-2019S1-frontend/src/actions/ModalViewActions.js
@@ -51,3 +51,11 @@ export function closeEventView() {
     type: ACTION_TYPE.CLOSE_EVENT_VIEW
   };
 }
+
+export function updateEvent(event) {
+  return {
+    type: ACTION_TYPE.UPDATE_EVENT,
+    event: event
+    // goodId: goodId
+  };
+}
