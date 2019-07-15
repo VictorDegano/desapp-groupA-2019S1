70acf9c1b2ec19170b2cd155c1f1f8e99a84fd7e
jaraivan <ivanjaratamargo@gmail.com>
2019-07-15 16:23:22 -0300

    adding updateBalance AccountAction

--- desapp-groupA-2019S1-frontend/src/actions/AccountActions.js
+++ desapp-groupA-2019S1-frontend/src/actions/AccountActions.js
@@ -1,11 +1,7 @@
-//Store
-import { store } from "../index";
 import * as ACTION_TYPE from "./Action_Types/AccountTypes";
 
 //Todas las funciones de acciones que hacemos llevan un type que es la accion que realiza y luego la informacion
 export function showAccount() {
-  // console.log('showLastEvents()');
-
   //Las acciones devuelve un objeto
   return {
     type: ACTION_TYPE.SHOW_ACCOUNT
@@ -13,10 +9,15 @@ export function showAccount() {
 }
 
 export function addMoney() {
-  // console.log('showLastEvents()');
-
   //Las acciones devuelve un objeto
   return {
     type: ACTION_TYPE.ADD_MONEY
+  };
+}
+
+export function updateBalance(balance) {
+  return {
+    type: ACTION_TYPE.UPDATE_BALANCE,
+    balance: balance
   };
 }
