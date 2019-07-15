517cc6bfe5968029f32244790c70cb8f0408b58a
IvanJara <ivanjaratamargo@gmail.com>
2019-07-15 17:46:05 -0300

    adding EXTRACT_MONEY AccountType

--- desapp-groupA-2019S1-frontend/src/actions/Action_Types/AccountTypes.js
+++ desapp-groupA-2019S1-frontend/src/actions/Action_Types/AccountTypes.js
@@ -2,3 +2,4 @@ export const SHOW_ACCOUNT = "SHOW_ACCOUNT";
 export const HIDE_ACCOUNT = "HIDE_ACCOUNT";
 export const ADD_MONEY = "ADD_MONEY";
 export const UPDATE_BALANCE = "UPDATE_BALANCE";
+export const EXTRACT_MONEY = "EXTRACT_MONEY";
