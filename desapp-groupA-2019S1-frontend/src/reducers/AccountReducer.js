e7c0b3e1e673d1592f43461de0705eb773b57aff
IvanJara <ivanjaratamargo@gmail.com>
2019-07-15 09:36:38 -0300

    adding AccountReducer with 2 reducers for now, to SHOW_ACCOUNT and to HIDE_ACCOUNT

---
+++ desapp-groupA-2019S1-frontend/src/reducers/AccountReducer.js
@@ -0,0 +1,29 @@
+//Todos los reducers tienen que tener un initial state, que no es el mismo que el initial state que se pueda generar en la aplicacion al iniciarla
+
+const initialState = {
+  showAccount: false
+};
+
+//Se recibe un state antiguo, un action y se devuelve el nuevo state
+export default function accountReducer(state = initialState, action) {
+  //Dependiendo del tipo de accion que recibimos es lo que vamos a realizar
+  switch (action.type) {
+    case "SHOW_ACCOUNT": {
+      // console.log('case SHOW_EVENTS_IN_PROGRESS');
+
+      return Object.assign({}, state, {
+        showAccount: true
+      });
+    }
+    case "HIDE_ACCOUNT": {
+      // console.log('case SHOW_EVENTS_IN_PROGRESS');
+
+      return Object.assign({}, state, {
+        showAccount: false
+      });
+    }
+
+    default:
+      return state;
+  }
+}
